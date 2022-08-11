1afeda0fac4bcf80a218769a63d7497f471c04ea
Viacheslav <davidov.ve@yandex.ru>
2022-08-11 11:39:17 +0300

    task 10

---
+++ task10_homework/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное чилсо: ");
+int number = int.Parse(Console.ReadLine());
+
+int num1 = number % 100;
+int num2 = num1 / 10;
+
+if ((number < 100) || (number > 1000))
+{
+    Console.Write($"Число {number} не является трёхзначным");
+}
+
+else
+{
+    Console.Write($"Вторая цифра числа {number}: {num2}");
+}
